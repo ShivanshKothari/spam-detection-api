2818eccb295b8e54b4ea2a8467fa7419ba099d35
shivanshkothari2004@gmail.com <shivanshkothari2004@gmail.com>
2024-10-04 13:24:27 +0530

    moved config and cmd folder

--- config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-const (
-	MongoURI  = "mongodb+srv://shivanshkothari:LearnKothari2004@cluster0.1ksbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-	DbName    = "spam_detection"
-	JwtSecret = "my-secret-key-875"
-)
