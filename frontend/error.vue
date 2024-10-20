658106b3b6b7ed2a78b677cdfcdafef93ef88f1a
shivanshkothari2004@gmail.com <shivanshkothari2004@gmail.com>
2024-10-20 22:25:27 +0530

    Updated 404 error message

--- frontend/error.vue
+++ frontend/error.vue
@@ -1,56 +1,19 @@
-<!-- <script setup lang="ts">
-import type { NuxtError } from "nuxt/app";
-
-const props = defineProps({
-	error: Object as () => NuxtError,
-});
-
-import { Rive, Layout } from "@rive-app/canvas";
-
-const name = "Rive";
-const src = String;
-const fit = String;
-const alignment = String;
-
-mounted(() => {
-	new Rive({
-		canvas: this.$refs.canvas,
-		src: this.$props.src,
-		layout: new Layout({
-      fit: this.$props.fit,
-      alignment: this.$props.alignment,
-    }),
-    autoplay: true,
-  });
-});
-</script>
-<template>
-  <div>
-    <NuxtLayout>
-    <div>
-    <canvas ref="canvas" class="w-full h-screen" ></canvas>
-  </div>
-  </NuxtLayout>
-    
-    <!-- <h1>{{ error?.statusCode }}</h1>
-    <NuxtLink to="/">Go back home</NuxtLink> -->
-  </div>
-</template> -->
 
 <template>
   <div>
-    <canvas ref="canvas" width="400" height="400"></canvas>
+    <canvas ref="canvas" height="1000" width="1000"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { NuxtError } from '#app';
 import { ref, onMounted } from 'vue';
-import { Rive } from '@rive-app/canvas';
+import { Rive } from '@rive-app/react-canvas';
 
-const props = defineProps<{
+const props = defineProps({
 	error: Object as () => NuxtError,
-  src: string;
-}>();
+  src: { type: String, required: true },
+});
 
 const canvas = ref<HTMLCanvasElement | null>(null);
 
@@ -58,8 +21,7 @@ onMounted(() => {
   if (canvas.value) {
     new Rive({
       canvas: canvas.value,
-      src: props.src,
-      autoplay: true,
+      src: '/llg_404_test.riv',
     });
   }
 });
